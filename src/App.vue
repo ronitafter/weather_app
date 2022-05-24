947c99f61673b9cc830de3daa88c62746b6180ad
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-05-24 22:20:39 +0300

    added style 2

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,16 @@
       <input type="text" name="" id="" 
       class="search-bar" placeholder="Search..."/>
     </div>
+    <div class="weather-wrap">
+      <div class="location-box">
+        <div class="location">Tel Aviv, Israel</div>
+        <div class="date">Tuesday 24 May 2022</div>
+      </div>
+      <div class="weather-box">
+        <div class="temp">15ºc</div>
+        <div class="weather">Rain</div>
+      </div>
+    </div>
   </main>
 </div>
 </template>
@@ -43,5 +53,36 @@ main{
 .search-box{
   width: 100%;
   margin-bottom: 30px;
+}
+
+.search-box .search-bar{
+  display:block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location{ 
+  color: #fff;
+  font-size: 25px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+
 }
 </style>
