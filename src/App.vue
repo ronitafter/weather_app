9ceeb5ea80ecf011f4076b2b35ca83f2c09899a6
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-05-24 12:46:05 +0300

    added style 1

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,28 @@ export default {
 
 }
 </script>
-
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: sans-serif, 'Times New Roman', Times, serif;
+}
+#app{
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+main{
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
 </style>
