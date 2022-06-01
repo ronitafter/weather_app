abf8b729344a24538f8cf4f85482af00b7fdb914
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-06-01 22:12:14 +0300

    fixed search box

--- src/App.vue
+++ src/App.vue
@@ -109,14 +109,14 @@ main{
   background: none;
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.5);
-  border-radius: 0px 16px 0px 16px;
+  border-radius: 8px;
   transition: 0.4s;
 }
 
 .search-box .search-bar:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 16px 0px 16px 0px;
+  
 }
 
 .location-box .location{ 
